c173c43c77b5a23b944a023743770ffd475f2e41
930237972@qq.com <930237972@qq.com>
2019-03-14 09:32:48 +0800

    初始版

---
+++ src/routes.js
@@ -0,0 +1,43 @@
+import Login from './views/Login.vue'
+import NotFound from './views/404.vue'
+import Home from './views/Home.vue'
+import Main from './views/Main.vue'
+import Table from './views/Table.vue'
+import Form from './views/nav1/Form.vue'
+import user from './views/nav1/user.vue'
+import Page4 from './views/nav2/Page4.vue'
+import Page5 from './views/nav2/Page5.vue'
+import Page6 from './views/nav3/Page6.vue'
+import echarts from './views/charts/echarts.vue'
+
+let routes = [
+    {
+        path: '/login',
+        component: Login,
+        name: '',
+        hidden: true
+    },
+    {
+        path: '/404',
+        component: NotFound,
+        name: '',
+        hidden: true
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '',
+        iconCls: 'fa fa-address-card',
+        leaf: true,//只有一个节点
+        children: [
+            { path: '/table', component: Table, name: '数据查询' }
+        ]
+    },
+    {
+        path: '*',
+        hidden: true,
+        redirect: { path: '/404' }
+    }
+];
+
+export default routes;
---
+++ src/views/Table.vue
@@ -0,0 +1,126 @@
+<template>
+	<section>
+		<!--工具条-->
+		<el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+			<el-form :inline="true" :model="filters">
+				<el-form-item>
+					<div class="block" style="width: 260px">
+						<el-date-picker
+								v-model="filters.name"
+								type="daterange"
+								align="right"
+								unlink-panels
+								range-separator="至"
+								start-placeholder="开始日期"
+								end-placeholder="结束日期"
+								:picker-options="pickerOptions2">
+						</el-date-picker>
+					</div>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" v-on:click="getUsers">查询</el-button>
+				</el-form-item>
+			</el-form>
+		</el-col>
+
+		<!--列表-->
+		<el-table :data="users" highlight-current-row v-loading="listLoading" @selection-change="selsChange" style="width: 100%;" stripe>
+			<el-table-column prop="date" label="日期" style="width: 50%;">
+			</el-table-column>
+			<el-table-column prop="count" label="数据" style="width: 50%;">
+			</el-table-column>
+		</el-table>
+
+		<!--工具条-->
+		<el-col :span="24" class="toolbar">
+			<el-pagination layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="20" :total="total" style="float:right;">
+			</el-pagination>
+		</el-col>
+
+	</section>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				pickerOptions2: {
+					shortcuts: [{
+						text: '最近一周',
+						onClick(picker) {
+							const end = new Date();
+							const start = new Date();
+							start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+							picker.$emit('pick', [start, end]);
+						}
+					}, {
+						text: '最近一个月',
+						onClick(picker) {
+							const end = new Date();
+							const start = new Date();
+							start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+							picker.$emit('pick', [start, end]);
+						}
+					}, {
+						text: '最近三个月',
+						onClick(picker) {
+							const end = new Date();
+							const start = new Date();
+							start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+							picker.$emit('pick', [start, end]);
+						}
+					}]
+				},
+				filters: {
+					name: ''
+				},
+				users: [],
+				total: 0,
+				page: 1,
+				listLoading: false,
+				sels: [],//列表选中列
+			}
+
+		},
+		methods: {
+			handleCurrentChange(val) {
+				this.page = val;
+				this.getUsers();
+			},
+			//获取用户列表
+			getUsers() {
+				let para = {
+					page: this.page,
+					name: this.filters.name
+				};
+
+				let myData = {
+					startTime: JSON.stringify(this.filters.name[0]),
+					endTime: JSON.stringify(this.filters.name[1]),
+					pageNumm: 1,
+				 	pageSize: 2
+				}
+				this.listLoading = true;
+				this.$http.post('http://47.93.225.228:8080/sysManager/getH5UserRegster', myData, {emulateJSON: true}).then(result => {
+					console.log(result)
+					this.total = result.body.data.total;
+					this.users = result.body.data.records;
+					this.listLoading = false;
+				})
+			},
+			selsChange: function (sels) {
+				this.sels = sels;
+			},
+			pickerOptions2: function() {},
+			formatSex: function() {},
+		},
+		mounted() {
+			this.getUsers();
+		}
+	}
+
+</script>
+
+<style scoped>
+
+</style>
